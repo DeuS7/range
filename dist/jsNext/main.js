101f54c71108445910c7670a3e8ffc4b1f38b899
Felix <never-99@yandex.com>
2019-02-02 02:54:00 +0500

    added meta

---
+++ dist/jsNext/main.js
@@ -0,0 +1,8 @@
+initField();
+
+//Allow to cache the sounds while preloader
+for (var key in sounds) {
+	makeSound(key, 0, 0);
+}
+/*hidePreloader(3);*/
+
---
+++ dist/jsNext/preloader.js
@@ -0,0 +1,8 @@
+function hidePreloader(delay) {
+	preloader.classList.add("removePreloader");
+	preloader.style.animationDelay = (delay - 0.5) + "s";
+	setTimeout(function() {
+		preloader.remove();
+	}, delay*1000);
+
+}
